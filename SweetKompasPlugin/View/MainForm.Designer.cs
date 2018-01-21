9b2ec3a9fb2c730d7ff3eae322bd15e90249d379
Anna Voln <vao5841@yandex.ru>
2018-01-21 22:19:10 +0700

    MainForm fix: All params are measured in millimeters

--- SweetKompasPlugin/View/MainForm.Designer.cs
+++ SweetKompasPlugin/View/MainForm.Designer.cs
@@ -57,7 +57,7 @@
             // 
             // CandyCountTextBox
             // 
-            this.CandyCountTextBox.Location = new System.Drawing.Point(184, 10);
+            this.CandyCountTextBox.Location = new System.Drawing.Point(197, 10);
             this.CandyCountTextBox.Name = "CandyCountTextBox";
             this.CandyCountTextBox.Size = new System.Drawing.Size(100, 20);
             this.CandyCountTextBox.TabIndex = 1;
@@ -69,22 +69,22 @@
             this.CandyCountLabel.AutoSize = true;
             this.CandyCountLabel.Location = new System.Drawing.Point(13, 13);
             this.CandyCountLabel.Name = "CandyCountLabel";
-            this.CandyCountLabel.Size = new System.Drawing.Size(106, 13);
+            this.CandyCountLabel.Size = new System.Drawing.Size(128, 13);
             this.CandyCountLabel.TabIndex = 2;
-            this.CandyCountLabel.Text = "Количество конфет";
+            this.CandyCountLabel.Text = "Количество конфет, мм";
             // 
             // CandyLengthLabel
             // 
             this.CandyLengthLabel.AutoSize = true;
             this.CandyLengthLabel.Location = new System.Drawing.Point(13, 43);
             this.CandyLengthLabel.Name = "CandyLengthLabel";
-            this.CandyLengthLabel.Size = new System.Drawing.Size(88, 13);
+            this.CandyLengthLabel.Size = new System.Drawing.Size(110, 13);
             this.CandyLengthLabel.TabIndex = 3;
-            this.CandyLengthLabel.Text = "Длина конфеты";
+            this.CandyLengthLabel.Text = "Длина конфеты, мм";
             // 
             // CandyLengthTextBox
             // 
-            this.CandyLengthTextBox.Location = new System.Drawing.Point(184, 40);
+            this.CandyLengthTextBox.Location = new System.Drawing.Point(197, 40);
             this.CandyLengthTextBox.Name = "CandyLengthTextBox";
             this.CandyLengthTextBox.Size = new System.Drawing.Size(100, 20);
             this.CandyLengthTextBox.TabIndex = 4;
@@ -96,13 +96,13 @@
             this.CandyWidthLabel.AutoSize = true;
             this.CandyWidthLabel.Location = new System.Drawing.Point(13, 73);
             this.CandyWidthLabel.Name = "CandyWidthLabel";
-            this.CandyWidthLabel.Size = new System.Drawing.Size(94, 13);
+            this.CandyWidthLabel.Size = new System.Drawing.Size(116, 13);
             this.CandyWidthLabel.TabIndex = 5;
-            this.CandyWidthLabel.Text = "Ширина конфеты";
+            this.CandyWidthLabel.Text = "Ширина конфеты, мм";
             // 
             // CandyWidthTextBox
             // 
-            this.CandyWidthTextBox.Location = new System.Drawing.Point(184, 70);
+            this.CandyWidthTextBox.Location = new System.Drawing.Point(197, 70);
             this.CandyWidthTextBox.Name = "CandyWidthTextBox";
             this.CandyWidthTextBox.Size = new System.Drawing.Size(100, 20);
             this.CandyWidthTextBox.TabIndex = 6;
@@ -114,13 +114,13 @@
             this.CandyHeightLabel.AutoSize = true;
             this.CandyHeightLabel.Location = new System.Drawing.Point(12, 103);
             this.CandyHeightLabel.Name = "CandyHeightLabel";
-            this.CandyHeightLabel.Size = new System.Drawing.Size(93, 13);
+            this.CandyHeightLabel.Size = new System.Drawing.Size(115, 13);
             this.CandyHeightLabel.TabIndex = 7;
-            this.CandyHeightLabel.Text = "Высота конфеты";
+            this.CandyHeightLabel.Text = "Высота конфеты, мм";
             // 
             // CandyHeightTextBox
             // 
-            this.CandyHeightTextBox.Location = new System.Drawing.Point(184, 100);
+            this.CandyHeightTextBox.Location = new System.Drawing.Point(197, 100);
             this.CandyHeightTextBox.Name = "CandyHeightTextBox";
             this.CandyHeightTextBox.Size = new System.Drawing.Size(100, 20);
             this.CandyHeightTextBox.TabIndex = 8;
@@ -132,13 +132,13 @@
             this.FormDepthByLengthLabel.AutoSize = true;
             this.FormDepthByLengthLabel.Location = new System.Drawing.Point(13, 133);
             this.FormDepthByLengthLabel.Name = "FormDepthByLengthLabel";
-            this.FormDepthByLengthLabel.Size = new System.Drawing.Size(140, 13);
+            this.FormDepthByLengthLabel.Size = new System.Drawing.Size(162, 13);
             this.FormDepthByLengthLabel.TabIndex = 9;
-            this.FormDepthByLengthLabel.Text = "Толщина формы по длине";
+            this.FormDepthByLengthLabel.Text = "Толщина формы по длине, мм";
             // 
             // FormDepthByLengthTextBox
             // 
-            this.FormDepthByLengthTextBox.Location = new System.Drawing.Point(184, 130);
+            this.FormDepthByLengthTextBox.Location = new System.Drawing.Point(197, 130);
             this.FormDepthByLengthTextBox.Name = "FormDepthByLengthTextBox";
             this.FormDepthByLengthTextBox.Size = new System.Drawing.Size(100, 20);
             this.FormDepthByLengthTextBox.TabIndex = 10;
@@ -150,13 +150,13 @@
             this.FormDepthByWidthLabel.AutoSize = true;
             this.FormDepthByWidthLabel.Location = new System.Drawing.Point(13, 163);
             this.FormDepthByWidthLabel.Name = "FormDepthByWidthLabel";
-            this.FormDepthByWidthLabel.Size = new System.Drawing.Size(148, 13);
+            this.FormDepthByWidthLabel.Size = new System.Drawing.Size(170, 13);
             this.FormDepthByWidthLabel.TabIndex = 11;
-            this.FormDepthByWidthLabel.Text = "Толщина формы по ширине";
+            this.FormDepthByWidthLabel.Text = "Толщина формы по ширине, мм";
             // 
             // FormDepthByWidthTextBox
             // 
-            this.FormDepthByWidthTextBox.Location = new System.Drawing.Point(184, 160);
+            this.FormDepthByWidthTextBox.Location = new System.Drawing.Point(197, 160);
             this.FormDepthByWidthTextBox.Name = "FormDepthByWidthTextBox";
             this.FormDepthByWidthTextBox.Size = new System.Drawing.Size(100, 20);
             this.FormDepthByWidthTextBox.TabIndex = 12;
@@ -168,13 +168,13 @@
             this.FormDepthByHeightLabel.AutoSize = true;
             this.FormDepthByHeightLabel.Location = new System.Drawing.Point(13, 193);
             this.FormDepthByHeightLabel.Name = "FormDepthByHeightLabel";
-            this.FormDepthByHeightLabel.Size = new System.Drawing.Size(147, 13);
+            this.FormDepthByHeightLabel.Size = new System.Drawing.Size(169, 13);
             this.FormDepthByHeightLabel.TabIndex = 13;
-            this.FormDepthByHeightLabel.Text = "Толщина формы по высоте";
+            this.FormDepthByHeightLabel.Text = "Толщина формы по высоте, мм";
             // 
             // FormDepthByHeightTextBox
             // 
-            this.FormDepthByHeightTextBox.Location = new System.Drawing.Point(184, 190);
+            this.FormDepthByHeightTextBox.Location = new System.Drawing.Point(197, 190);
             this.FormDepthByHeightTextBox.Name = "FormDepthByHeightTextBox";
             this.FormDepthByHeightTextBox.Size = new System.Drawing.Size(100, 20);
             this.FormDepthByHeightTextBox.TabIndex = 14;
@@ -185,7 +185,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 251);
+            this.ClientSize = new System.Drawing.Size(312, 251);
             this.Controls.Add(this.FormDepthByHeightTextBox);
             this.Controls.Add(this.FormDepthByHeightLabel);
             this.Controls.Add(this.FormDepthByWidthTextBox);
