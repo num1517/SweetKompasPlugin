1b23f4ed1b8379a32696e3d0272953c675590d7e
janewoln <anna.o.voloshina@gmail.com>
2018-01-21 20:08:07 +0700

    Unit tests project added.

---
+++ SweetTest/CandyFormTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace SweetTest
+{
+    [TestFixture]
+    public class CandyFormTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
